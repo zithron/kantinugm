75d6b11d6f0d755b32e709089cd8a2341574ef7d
zithron <abdulbasithb70@gmail.com>
2017-05-17 16:37:01 +0700

    commit pertama

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('ul.tabs').tabs();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
